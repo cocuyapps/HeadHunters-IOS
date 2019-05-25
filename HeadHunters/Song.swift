b1890e9336057e55a44622ec20483932027403e1
System Administrator <root@Diegos-MacBook-Pro.local>
2019-05-25 15:56:27 -0500

    added swift classes

---
+++ HeadHunters/Song.swift
@@ -0,0 +1,16 @@
+//
+//  Song.swift
+//  HeadHunters
+//
+//  Created by Diego Salas Noain on 5/25/19.
+//  Copyright © 2019 CoCuyApps. All rights reserved.
+//
+
+import Foundation
+
+struct Song: Codable {
+    var artist: String?
+    var title: String?
+    var albumArtUrl: String?
+    var audioUrl: String?
+}
