17bb4d91d7f2344fff1e965c598fb76f935679d5
Miguel Delgado <miguel.9dc@gmail.com>
2019-06-08 15:04:50 -0500

    genre model and view added

---
+++ HeadHunters/HeadHunters/Main/Genre.swift
@@ -0,0 +1,14 @@
+//
+//  Genre.swift
+//  HeadHunters
+//
+//  Created by Alumnos on 6/8/19.
+//  Copyright © 2019 CoCuyApps. All rights reserved.
+//
+
+import Foundation
+
+struct Genre: Codable{
+    var genre: String?
+    var genreimg: String?
+}
