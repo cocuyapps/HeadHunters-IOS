dbbe0e00347bd930709e2a8f8f674bfeeadc3bc1
diegosalas27 <dominicsc2hs@gmail.com>
2019-06-18 20:45:17 -0500

    finished media player

--- HeadHunters/HeadHunters/ViewControllers/MediaPlayerViewController.swift
+++ HeadHunters/HeadHunters/ViewControllers/MediaPlayerViewController.swift
@@ -20,9 +20,9 @@ class MediaPlayerViewController: UIViewController {
     @IBOutlet weak var artImageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var artistLabel: UILabel!
-    @IBOutlet weak var PausePlayButton: UIButton!
-    @IBOutlet weak var nextButton: UIButton!
-    @IBOutlet weak var previousButton: UIButton!
+    @IBOutlet weak var playPauseBtn: UIButton!
+    @IBOutlet weak var skipNextBtn: UIButton!
+    @IBOutlet weak var skipPreviousBtn: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,6 +34,56 @@ class MediaPlayerViewController: UIViewController {
         setPlayer()
     }
     
+    @IBAction func PlayPause(_ sender: UIButton) {
+        if player?.rate == 0
+        {
+            player!.play()
+            playPauseBtn.setBackgroundImage(UIImage(named: "ic_pause_white_48pt"), for: .normal)
+        } else {
+            player!.pause()
+            playPauseBtn.setBackgroundImage(UIImage(named: "ic_play_arrow_white_48pt"), for: .normal)
+        }
+    }
+    
+    @IBAction func skipNext(_ sender: UIButton) {
+        if(currentSong < songs.count - 1){
+            currentSong = currentSong + 1;
+            player!.pause()
+            player = nil
+            
+            setPlayer();
+            if player?.rate == 0
+            {
+                titleLabel.text = "Loading..."
+                artistLabel.text = "Loading..."
+                player!.play()
+                playPauseBtn.setBackgroundImage(UIImage(named: "ic_pause_white_48pt"), for: .normal)
+                titleLabel.text = songs[currentSong].title
+                artistLabel.text = songs[currentSong].artist
+            }
+        }
+    }
+    
+    
+    @IBAction func skipPrevious(_ sender: UIButton) {
+        if(currentSong > 0){
+            currentSong = currentSong - 1;
+            player!.pause()
+            player = nil
+            
+            setPlayer();
+            if player?.rate == 0
+            {
+                titleLabel.text = "Loading..."
+                artistLabel.text = "Loading..."
+                player!.play()
+                playPauseBtn.setBackgroundImage(UIImage(named: "ic_pause_white_48pt"), for: .normal)
+                titleLabel.text = songs[currentSong].title
+                artistLabel.text = songs[currentSong].artist
+            }
+        }
+    }
+    
     func setPlayer() {
         let url = URL(string: songs[currentSong].audioUrl!)!
         let playerItem: AVPlayerItem = AVPlayerItem(url: url)
@@ -41,5 +91,26 @@ class MediaPlayerViewController: UIViewController {
         
         playerLayer=AVPlayerLayer(player: player!)
         self.view.layer.addSublayer(playerLayer!)
+    }
+    
+    func contPlay(){
+        
+        if(currentSong < songs.count - 1){
+            currentSong = currentSong + 1
+            player!.pause()
+            player = nil
+            
+            setPlayer();
+            if player?.rate == 0
+            {
+                titleLabel.text = "Loading..."
+                artistLabel.text = "Loading..."
+                player!.play()
+                playPauseBtn.setBackgroundImage(UIImage(named: "ic_pause_white_48pt"), for: .normal)
+                titleLabel.text = songs[currentSong].title
+                artistLabel.text = songs[currentSong].artist
+            }
+            
+        }
     }
 }
