bfae111296be766b1fa32805ae4de147cbe67462
diegosalas27 <dominicsc2hs@gmail.com>
2019-06-18 19:33:18 -0500

    changes

--- HeadHunters/HeadHunters/ViewControllers/MediaPlayerViewController.swift
+++ HeadHunters/HeadHunters/ViewControllers/MediaPlayerViewController.swift
@@ -7,16 +7,22 @@
 //
 
 import UIKit
+import AVFoundation
 
 class MediaPlayerViewController: UIViewController {
     var songs: [Song] = [Song]()
     
+    var player: AVPlayer!
+    var playerItem: AVPlayerItem!
+    var playerLayer: AVPlayerLayer!
+    var currentSong = 0
+    
     @IBOutlet weak var artImageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var artistLabel: UILabel!
-    @IBOutlet weak var skipBackImageView: UIImageView!
-    @IBOutlet weak var playPauseImageView: UIImageView!
-    @IBOutlet weak var skipNextImageView: UIImageView!
+    @IBOutlet weak var PausePlayButton: UIButton!
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var previousButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,5 +31,15 @@ class MediaPlayerViewController: UIViewController {
                                 withErrorImage: "no-image-available")
         titleLabel.text = songs[0].title
         artistLabel.text = songs[0].artist
+        setPlayer()
+    }
+    
+    func setPlayer() {
+        let url = URL(string: songs[currentSong].audioUrl!)!
+        let playerItem: AVPlayerItem = AVPlayerItem(url: url)
+        player = AVPlayer(playerItem: playerItem)
+        
+        playerLayer=AVPlayerLayer(player: player!)
+        self.view.layer.addSublayer(playerLayer!)
     }
 }
